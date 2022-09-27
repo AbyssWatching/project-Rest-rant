ea9d519a42dc432c2f1c53e3f53caa76e320dc6f
Winston Grant <winstonf.grant@hotmail.com>
2022-09-27 13:39:28 -0700

    fancied up the nav

--- Views/default.jsx
+++ Views/default.jsx
@@ -8,6 +8,13 @@ function Def (html) {
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"/>
             <link rel="stylesheet" href="/CSS/style.css" />
         </head>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/places">Places</a></li>
+                <li><a href="/places/new">Add Place</a></li>
+            </ul>
+        </nav>
         <body>
             {html.children}
         </body>
