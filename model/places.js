637f0d96b032ae0d7e364a9cec8f1e3bd7030d86
Winston Grant <winstonf.grant@hotmail.com>
2022-10-02 21:43:34 -0700

    I made it work lets go

--- model/places.js
+++ model/places.js
@@ -1,14 +1,11 @@
-module.exports = [{
-    name: "H-Thai-ML",
-    city: "Seattle",
-    state: "wa",
-    cuisines: "Thai, Pan-Asian",
-    pic: "/images/animeResteraunt.webp",
-    },
-{
-    name: "Coding Cat Cafe",
-    city: "Phoenix",
-    state: "AZ",
-    cuisines: "Coffee, Bakery",
-    pic: "/images/Resterauntanime.webp",
-}]
+const mongoose = require("mongoose")
+const placeSchema = new mongoose.Schema({
+    name:{ type:String, required: true},
+    pic: String,
+    cuisines: {type: String, required: true},
+    city: { type: String, default:"Anytime"},
+    state: {type:String, default:"USA"},
+    founded: Number
+})
+
+module.exports = mongoose.model("Place", placeSchema)
